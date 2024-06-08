1ba9d66a3fb361c86010f52808537e8f0cf567ac
gaeju <100760127+gaeju@users.noreply.github.com>
2024-06-08 18:03:55 +0900

    [level 0] Title: 짝수의 합, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,7 +1,6 @@
-def solution(n):
+def solution(n): # n = [1,2,3,4,5,6,7,8,9,10]
     s = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if i % 2 == 0:
             s += i
-
     return s
